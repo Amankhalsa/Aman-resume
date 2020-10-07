874760ebc68748ebf46ab522a7dc6b41af8a253c
Amankhalsa <66160544+Amankhalsa@users.noreply.github.com>
2020-10-07 15:12:29 +0530

    Add files via upload

---
+++ resume.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Amanpreet singh</title>
+	<style type="text/css">
+		
+*{width:95%;
+margin-left: 2%;
+margin-right: 2%;
+font-weight: bold;
+text-align: word-wrap;
+}
+		ul li{font-size: 20px; color:green;}
+		ul {font-size: 20px; color:navy;}
+		.footer{
+			 color: red;
+		}
+
+		footer ul{
+			color:green;
+			font-weight: bolder;
+		}
+		.address ul li{
+			display: block;
+		}
+
+		body{
+			text-align: word-wrap;
+		
+			overflow: auto;
+			width:95%;
+			margin: 2%;
+
+		}
+	</style>
+
+</head>
+<body>
+	<p>
+<?php //Title
+        echo   "<div style='text-align:center; color:red; font-size:3em; font-weight:bolder'>Resume</div>'<hr> "
+			  ;?>		
+
+<?=
+// Address
+  " 
+ <div class='address'>
+<ul >  
+<h1><span style='color:blue; text-align:center'>AmanPreet Singh</span></h1>
+
+<li>+91 98774 97776</li>
+<li>+91 97794 77625</li>
+<li># 415, sec-2, block- B, Adarsh Nagar </li>
+<li>Mandi Gobindgarh  147301(PB)</li>
+<li>Amansin61@gmail.com</li>
+<li><a href='#' > Web site- Amansin.weebly.com</a></ul></div>
+ ";?>
+
+<?=
+# Objective 
+"<div <div 75 '>";?>
+<!-- Objective -->
+
+<?="
+<ul type='circle'> 
+<h1>Objectives</h1><hr>
+<li>To work in an organization which provides me ample learning opportunities, a professional status and a wider 
+<br>
+experience to accomplish mine as well as company’s professional growth.</li>
+
+<h1>Education</h1>
+<li>	10+2 from Punjab school education board Mohali in the year 2010. </li>
+<li>	Completed Diploma in Computer Hardware & Networking Engineering from JETKING  Ludhiana in the year 2015.</li>
+<li>	I have done Bachelor of Science (Information Technology) (Bsc IT) from LPU.</li></div> " 
+// Education ;?>
+
+<?=  
+// Experience 
+"<div  >
+<ul TYPE='circle'><h1>Experience</h1>
+<li>	2015 - 2017 | Computer Teacher &computer Technical      Administrator  in Gyan Niketan Public School  |Dhandari Khurd, Ludhiana |. </li>
+
+<li>	(2017 -2019) Diya International Sen. Sec. School as a Computer Teacher as well as IT coordinator.</li> 
+</div>
+";?>
+
+<?=
+"
+<div>
+<ul type='circle' >
+<h2>Skills</h2>
+	<li>Computer Applications installation & MS Office Skills.</li>
+	<li>Basic Computer Electronics Skills.</li>
+	<li>PC Hardware Support Skills.</li>
+	<li>PC Assembling & Dissembling Skills.</li>
+	<li>Operating System Installation  and  Troubleshooting Skills.</li>
+	<li>Networking Essentials Skills.</li>
+	<li>MCITP (SERVER 2008   R2 Administration ) Skills</li>
+	<li>CCNA Skills & BASIC LINUX Skills.</li>
+	<li>Basic C,C++, Html language, PHP, SQL,Java or Javascript & web Designing skills.</li>
+	<li>ERP system skills.</li>
+	<li>Online Fess & library  Management Software skills.</li>
+
+
+<h2>PERSONAL DETAILS</h2>
+<li>Name			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	:	Amanpreetsingh</li>
+<li>Date of Birth	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	:	09-01-1992</li>
+<li>Father's Name	&nbsp;&nbsp;&nbsp;	:	S. Amarjit Singh</li>
+<li>Gender			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	:	Male</li>
+<li>Marital Status	&nbsp;&nbsp;&nbsp;	:	unmarried</li>
+<li>Nationality		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	:	Indian</li>
+<li>Strengths		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;	:	Good Communication Skills, Adjustable and Optimist</li>
+<li>Hobbies			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;	:	Reading Magazines, Surfing on  Internet and many more</li>
+<li>Languages Known	&nbsp;	:	English, Punjabi and Hindi</li>
+ 
+<span class='footer'>
+<H3>DECLARATION</H3>
+<h4>I hereby declare that all the information given above is correct according to the best of my knowledge and belief.</H4>
+
+</ul></div> 
+</span>
+"?>
+
+<?=
+"
+
+<ul>
+ 
+<p style='text-align:left'>Place:Mandi Gobindgarh     </p>
+
+<p>
+Date: 06-Oct-2020</p></ul>
+
+";?>
+
+
+</p>
+</body>
+</html>
+
